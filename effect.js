d1308d0e6b3f392baf84ce00453cba0fe23c8609
vigneshkp90 <vigneshkp90@gmanil.com>
2021-05-13 19:45:44 +0530

    Add files via upload

--- effect.js
+++ effect.js
@@ -173,7 +173,7 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(1200).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(100).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
 			if(i==50){
